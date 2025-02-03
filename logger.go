bf0cd010eca3d6f352792e824220fc48cbd7cfc9
vicky.phang <vickyphang11@gmail.com>
2025-02-03 14:49:08 +0700

    initial commit

---
+++ logger.go
@@ -0,0 +1,108 @@
+package gin_logger
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	gologging "github.com/devopsfaith/krakend-gologging"
+	logstash "github.com/devopsfaith/krakend-logstash"
+	"github.com/gin-gonic/gin"
+	"github.com/luraproject/lura/config"
+	"github.com/luraproject/lura/logging"
+)
+
+const (
+	Namespace  = "github_com/vickyphang/krakend-custom-log"
+	moduleName = "krakend-custom-log"
+)
+
+func NewLogger(cfg config.ExtraConfig, logger logging.Logger, loggerConfig gin.LoggerConfig) gin.HandlerFunc {
+	v, ok := ConfigGetter(cfg).(Config)
+	if !ok {
+		return gin.LoggerWithConfig(loggerConfig)
+	}
+
+	loggerConfig.SkipPaths = v.SkipPaths
+	logger.Info(fmt.Sprintf("%s: total skip paths set: %d", moduleName, len(v.SkipPaths)))
+
+	loggerConfig.Output = ioutil.Discard
+	loggerConfig.Formatter = Formatter{logger, v}.DefaultFormatter
+	return gin.LoggerWithConfig(loggerConfig)
+}
+
+type Formatter struct {
+	logger logging.Logger
+	config Config
+}
+
+func (f Formatter) DefaultFormatter(params gin.LogFormatterParams) string {
+
+	header := params.Request.Header
+	body := params.Request.Body
+
+	record := map[string]interface{}{
+		"method":             params.Method,
+		"host":               params.Request.Host,
+		"path":               params.Path,
+		"status_code":        params.StatusCode,
+		"user_agent":         params.Request.UserAgent(),
+		"client_ip":          params.ClientIP,
+		"latency":            params.Latency,
+		"response_timestamp": params.TimeStamp,
+		"body":               body,
+		"header":             header,
+	}
+
+	payload := map[string]interface{}{
+		"data": record,
+	}
+
+	if f.config.Logstash {
+		f.logger.Info("", payload)
+	} else {
+		p, _ := json.Marshal(payload)
+		f.logger.Info(string(p))
+	}
+
+	return ""
+}
+
+func ConfigGetter(e config.ExtraConfig) interface{} {
+	v, ok := e[Namespace]
+	if !ok {
+		return nil
+	}
+	tmp, ok := v.(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	cfg := defaultConfigGetter()
+	if skipPaths, ok := tmp["skip_paths"].([]interface{}); ok {
+		var paths []string
+		for _, skipPath := range skipPaths {
+			if path, ok := skipPath.(string); ok {
+				paths = append(paths, path)
+			}
+		}
+		cfg.SkipPaths = paths
+	}
+	if v, ok = e[gologging.Namespace]; ok {
+		_, cfg.Logstash = e[logstash.Namespace]
+	}
+
+	return cfg
+}
+
+func defaultConfigGetter() Config {
+	return Config{
+		SkipPaths: []string{},
+		Logstash:  false,
+	}
+}
+
+type Config struct {
+	SkipPaths []string
+	Logstash  bool
+}
